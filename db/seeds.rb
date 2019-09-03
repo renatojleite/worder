72b36dfea8e7915b23d39ebf54bfdfad30f9083e
Renato Nobrega <renatojleite@gmail.com>
2019-09-03 12:11:42 -0300

    after sign in application - admin/user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
+User.destroy_all
+
 puts "Creating Users...."
-User.create!(email:"renatoMoloko@molokito.com", password: "123123" )
-User.create!(email: "contato1@banana.com", password: "123456" )
-User.create!(email: "contato2@banana.com", password: "123456" )
-User.create!(email: "contato3@banana.com", password: "123456" )
-User.create!(email: "contato4@banana.com", password: "123456" )
+user = User.create!(email: "user@mail.com", password: "123456" )
+puts "Users created!.."
+
+admin = User.create!(email: "admin@mail.com", password: "123456" )
+
+puts "Creating Manager...."
+manager = Manager.create!(user: admin)
+puts "Manager created!.."
+
+
+puts "Creating team leader ...."
+team_leader = TeamLeader.create! ( user: user, manager: manager )
+puts "Team leader created!.."
+
+
+
+puts "Users created: #{User.count}"
+puts "Manager created: #{Manager.count}"
+puts "Team leader created: #{TeamLeader.count}"
