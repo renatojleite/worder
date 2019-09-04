db90851f2a3d785ca2e96af5ab2cb889349c70b9
Carlos Roberto <carlosbuanaoda@gmail.com>
2019-09-04 14:58:49 -0300

    created first select filter

--- app/controllers/manager/work_orders_controller.rb
+++ app/controllers/manager/work_orders_controller.rb
@@ -2,11 +2,11 @@ class Manager::WorkOrdersController < ApplicationController
   before_action :set_work_order, only: %i[show edit update delete]
 
   def index
-    if params[:query].present?
-      @work_orders = WorkOrder.where("name ILIKE ?", "%#{params[:query]}%")
-    else
-      @work_orders = WorkOrder.all
-    end
+    @team_leaders = TeamLeader.all
+    @work_orders = WorkOrder.all
+
+    @work_orders = WorkOrder.where("name ILIKE ?", "%#{params[:query]}%") if params[:query].present?
+    @work_orders = WorkOrder.where(team_leader_id: params[:team]) if params[:team].present?
   end
 
   def new
--- app/models/team_leader.rb
+++ app/models/team_leader.rb
@@ -2,4 +2,9 @@ class TeamLeader < ApplicationRecord
   belongs_to :user
   belongs_to :manager
   has_many :work_orders
+
+  def get_team_name
+    "#{team_name}"
+  end
+
 end
