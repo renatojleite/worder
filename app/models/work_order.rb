56e7f37ca3a52325ce92f55027cb6c58edee04d9
Carlos Roberto <carlosbuanaoda@gmail.com>
2019-09-05 14:29:48 -0300

    fixed filters repeat

--- app/models/work_order.rb
+++ app/models/work_order.rb
@@ -2,6 +2,7 @@ class WorkOrder < ApplicationRecord
   # enum status: { Aberta: 0, 'Em andamento': 1, Concluido: 2 }
   belongs_to :team_leader
   geocoded_by :address
+  validates :name, uniqueness: true
   after_validation :geocode, if: :will_save_change_to_address?
   mount_uploader :photo, PhotoUploader
 end
