46509852064893609203d3e40e5a0ee46525c806
Carlos Roberto <carlosbuanaoda@gmail.com>
2019-09-05 15:11:45 -0300

    fixed stuff

--- app/controllers/manager/work_orders_controller.rb
+++ app/controllers/manager/work_orders_controller.rb
@@ -5,10 +5,12 @@ class Manager::WorkOrdersController < ApplicationController
     @team_leaders = TeamLeader.all
     @work_orders_full = WorkOrder.all
     @work_orders = @work_orders_full
+
     @work_orders = WorkOrder.where("name ILIKE ?", "%#{params[:query]}%") if params[:query].present?
     @work_orders = WorkOrder.where(team_leader_id: params[:team]) if params[:team].present?
     @work_orders = WorkOrder.where(priority: params[:priority]) if params[:priority].present?
     @work_orders = WorkOrder.where(status: params[:status]) if params[:status].present?
+    @work_orders = WorkOrder.where(due_date: params[:due_date]) if params[:due_date].present?
 
     @events = WorkOrder.where.not(due_date: nil).map { |w| { title: w.name, start: w.due_date } }
   end
