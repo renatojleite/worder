0c737806cb333c80058a5994e456760f12d6e7b1
carolin santoro <carolin_santoro@yahoo.com.br>
2019-09-09 18:19:23 -0300

    filtro corrigido

--- app/controllers/manager/dashboard_controller.rb
+++ app/controllers/manager/dashboard_controller.rb
@@ -7,10 +7,10 @@ class Manager::DashboardController < ApplicationController
     @work_orders = @work_orders_full
 
     @work_orders = WorkOrder.where("name ILIKE ?", "%#{params[:query]}%") if params[:query].present?
-    @work_orders = WorkOrder.where(team_leader_id: params[:team]) if params[:team].present?
-    @work_orders = WorkOrder.where(priority: params[:priority]) if params[:priority].present?
-    @work_orders = WorkOrder.where(status: params[:status]) if params[:status].present?
-    @work_orders = WorkOrder.where(due_date: params[:due_date]) if params[:due_date].present?
+    @work_orders =  @work_orders.where(team_leader_id: params[:team]) if params[:team].present?
+    @work_orders =  @work_orders.where(priority: params[:priority]) if params[:priority].present?
+    @work_orders =  @work_orders.where(status: params[:status]) if params[:status].present?
+    @work_orders =  @work_orders.where(due_date: params[:due_date]) if params[:due_date].present?
 
     # WORK ORDERS - Calendario
     @events = WorkOrder.where.not(due_date: nil).map { |w| { title: w.name, start: w.due_date } }
